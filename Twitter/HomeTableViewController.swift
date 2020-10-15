08509680ab82ca201e8a5926624e97cc640a6998
Evelyn Hasama <ehasama11@gmail.com>
2020-10-15 01:17:17 -0700

    New Features

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -17,21 +17,25 @@ class HomeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweets()
         
         myRefereshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefereshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
         
     }
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    }
     
     @objc func loadTweets(){
         
-        numberOfTweets = 20 as Int
+        numberOfTweets = 20
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myParams = ["count": numberOfTweets]
         
-        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams as [String : Any], success: { (tweets: [NSDictionary]) in
             
             self.tweetArray.removeAll()
             for tweet in tweets {
@@ -42,7 +46,7 @@ class HomeTableViewController: UITableViewController {
             self.myRefereshControl.endRefreshing()
             
         }, failure: { (Error) in
-            print("Could not retrwieve tweets.")
+            print("Could not retrieve tweets.")
         })
         
     }
@@ -54,7 +58,7 @@ class HomeTableViewController: UITableViewController {
         numberOfTweets = numberOfTweets + 20
         let myParams = ["count": numberOfTweets]
         
-        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams as [String : Any], success: { (tweets: [NSDictionary]) in
             
             self.tweetArray.removeAll()
             for tweet in tweets {
@@ -65,7 +69,7 @@ class HomeTableViewController: UITableViewController {
             self.myRefereshControl.endRefreshing()
             
         }, failure: { (Error) in
-            print("Could not retrwieve tweets.")
+            print("Could not retrieve tweets.")
         })
         
     }
@@ -100,8 +104,11 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
         
-        
+        cell.setFavorite(isFavorited: tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(isRetweeted: tweetArray[indexPath.row]["retweeted"] as! Bool)
         return cell
+        
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,65 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Evelyn Hasama on 10/14/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController, UITextViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tweetTextView.becomeFirstResponder()
+        tweetTextView.delegate = self
+        tweetTextView.text = ""
+        characterCount.text = "280"
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var characterCount: UILabel!
+    
+    @IBOutlet weak var tweetTextView: UITextView!
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let characterLimit = 280
+        let newText = NSString(string: tweetTextView.text!).replacingCharacters(in: range, with: text)
+        self.characterCount.text = String(280 - newText.count)
+        return newText.count < characterLimit
+        
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated:true, completion: nil)
+    }
+    
+    @IBAction func tweet(_ sender: Any) {
+        if (!tweetTextView.text.isEmpty) {
+            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
+                self.dismiss(animated: true, completion: nil)
+            }, failure: { (error) in
+                print("Error posting tweet \(error)")
+                self.dismiss(animated: true, completion: nil)
+            })
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
